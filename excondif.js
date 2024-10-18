15a70884718e3fd3cbbfc6f7a298694bae62034b
xinrong1234 <xinrongyang06@gmail.com>
2024-10-19 00:01:54 +0800

    primers arxius

---
+++ excondif.js
@@ -0,0 +1,14 @@
+//16.1.- Escriu, fent servir la instrucció for, un programa que escrigui a la consola 10 cops el text “Estem fent bucles”.
+
+for (let i = 0; i < 10; i++) {
+    console.log("estem fer bucles");
+    
+}
+
+let j=1;
+
+while (j<10 ) {
+    console.log("estem fer bucles</br>");
+    document.getElementByID("respostal").innerhtml=text; 
+    j++;
+}
